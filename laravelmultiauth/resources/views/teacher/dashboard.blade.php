a733acadc62bb2ccacf32098692310b0afcf9bbd
Jamil454 <168254533+Jamil454@users.noreply.github.com>
2024-11-14 13:46:07 +0600

    mutli_login

---
+++ laravelmultiauth/resources/views/teacher/dashboard.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container mt-5">
+        <div class="row">
+
+            <h1>Welcome to Teacher Dashboard</h1>
+        </div>
+    </div>
+
+    <form action="{{}}"></form>
+</body>
+</html>
