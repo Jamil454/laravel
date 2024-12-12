3cebc0eea03c07fb41436d09fa784aea3fe0766a
Jamil454 <168254533+Jamil454@users.noreply.github.com>
2024-12-12 13:11:48 +0600

    12-Dec-2024 Mail-send

---
+++ laravel_send_mail/resources/views/emails/demoMail.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>ItsolutionStuff.com</title>
+
+</head>
+
+<body>
+
+    <h1>{{ $mailData['title'] }}</h1>
+
+    <p>{{ $mailData['body'] }}</p>
+
+
+
+    <p>Hei Jonny ! How are You ?</p>
+
+
+
+    <p>Thank you</p>
+
+</body>
+
+</html>
